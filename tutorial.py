9795a0466ab1c55e4b429dfc9a68f4fa8648aa9b
Michał Przybylak <michal.przybylak@gmail.com>
2020-04-03 19:18:55 +0200

    read csv from file

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,22 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
+
+
 fileName = 'rdu-weather-history.csv'
 
 sc = SparkContext("local", "First App")
-file = sc.textFile(fileName, minPartitions=None, use_unicode=True)
+spark = SparkSession(sc)
 
+# print helps to see where application output starts
 print("!!!!\n\n\n")
-print(file)
+
+file = spark.read.load(fileName, format='csv', sep=";", header="true")
+
+print(file.take(1))
+file.show()
+
+# print helps to see where application output ends
 print("\n\n\n!!!!")
+
+spark.stop()
